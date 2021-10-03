5fdcb24e3e4b2afc98a130f53b40de68ff22da6f
jelena002 <jelena.radivojevic002@gamil.com>
2021-10-03 12:46:06 -0400

    Updated new file 3

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -1,11 +1,14 @@
 package setup;
 
+
 public class Third {
 
     public static void main(String[] args) {
         System.out.println("Jelena je carica");
         System.out.println("Jelena je carica");
         System.out.println("Ja sam najbolja");
+
+        System.out.println("Jelena Jelena");
 
 
     }
