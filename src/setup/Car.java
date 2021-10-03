ce1a70fa14db1abc0e4f8e3dfdebcdceaace79e2
jelena002 <jelena.radivojevic002@gamil.com>
2021-10-03 16:22:07 -0400

    Updating master to origin

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,13 +2,12 @@ package setup;
 
 public class Car {
 
-<<<<<<< HEAD
     // This is car from Master Brunch
 
     //we will have conflict
 
-    // becuse there is  different text in the different brunches
-=======
+    // because there is  different text in the different brunches
+
     // this is my car from James branch
 
     // more text
@@ -16,5 +15,6 @@ public class Car {
     // more text
 
 
->>>>>>> james
+
+
 }
